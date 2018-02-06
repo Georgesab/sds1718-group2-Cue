4cdda29411cd0bd85c0acbbcf260472377d4c739
George Sabourin <gcs516@student.bham.ac.uk>
2018-02-06 22:45:06 +0000

    Queue numbers adjust when removing users from queue

--- server/index.js
+++ server/index.js
@@ -19,9 +19,8 @@ connection.connect();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// GET request
+// GET request -- webpage
 app.get('/', (request, response) => {
-	//response.send('hello\n')
 	response.sendFile("public/index.html");
 })
 
@@ -31,16 +30,18 @@ app.post("/addToQueue", (request, response) => {
 	var user_id = request.body.user_id;
 	var number_players = request.body.num_players;
 
-	var sql = "INSERT INTO `users` (user_id, time_added, number_players) VALUES (?, ?, ?)";
+	var sql = "INSERT INTO `users` (user_id, time_added, number_players, queue_pos) VALUES (?, ?, ?, (SELECT max(queue_pos) FROM (SELECT * FROM `users`) AS bob)+1)";
 	var inserts = [user_id, new Date(), number_players];
 	sql = SQL.format(sql, inserts);
 
 	connection.query(sql, (err, result) => {
 		if(err) {
+			// Debugging
 			console.log(err);	
 		}
 		console.log("User '" + user_id + "' added to queue!");
 	});
+
 	response.sendStatus(200);
 })
 
@@ -48,13 +49,24 @@ app.post("/addToQueue", (request, response) => {
 // TODO: Change or add a request to only delete the front of the queue
 app.delete('/queue/delete', (request, response) => {
 
+	let pos;
 	var user_to_delete = request.body.userid;
 
-	response.send('Deleting user' + user_to_delete);
-
-	connection.query("DELETE FROM `users` WHERE (userid) = (?)", user_to_delete, (err,result) => {
+	//response.send('Deleting user' + user_to_delete);
+	
+	// Reduce queue position by 1 for all items after deleted
+	connection.query("UPDATE `users` SET queue_pos = queue_pos-1 WHERE queue_pos > (SELECT queue_pos FROM ((SELECT * FROM `users`) AS queue_del) WHERE user_id = ?)", user_to_delete, (err,result) => {
+		if(err) throw err;
+	});
+	
+	// Delete user from queue
+	connection.query("DELETE FROM `users` WHERE user_id = (?)", user_to_delete, (err,result) => {
 		if (err) throw err;
 	});
+
+	// Send 200 status
+	response.sendStatus(200);
+
   })
 
 
@@ -71,7 +83,7 @@ app.get('/queue', (request,response) => {
 // Start listening on the current port
 app.listen(port, (err) => {
 	if(err) {
-		return console.log('something bad', err)
+		return console.log('Something bad happened', err)
 	}
 
 	console.log("Server started. Now listening on: " + port);
